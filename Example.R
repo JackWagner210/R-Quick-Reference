e7d322b27203bfda525f89b276f6386fac8a4934
Jack Wagner <jrw210@txstate.edu>
2017-12-06 21:13:35 -0600

    Update Example.R

--- Example.R
+++ Example.R
@@ -1,1 +1,21 @@
+#?(function) Help for that function
 
+?c
+  
+?args
+  
+  
+varName <- data # sets variables
+
+x <- 2
+  
+ 
+old.dir <- getwd()
+
+
+getwd() # Shows current directory / file path
+
+ls() # Shows all objects in current directory.
+
+list.files()
+dir() # Shows all files in current directory
